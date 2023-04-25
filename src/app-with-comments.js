e71aa47d07b357cf90c11752f4a5183235f11302
jordyGafas <39955874+jordyGafas@users.noreply.github.com>
2023-04-25 13:53:05 +0200

    Create app-with-comments.js

---
+++ src/app-with-comments.js
@@ -0,0 +1,209 @@
+import './App.css';
+import {useEffect, useState, Fragment} from "react";
+
+
+//General tips / improvements
+// - Add loading state + show a loader when data is fetching
+// - Functions like formatForecast should be defined out of component or can even be in a different file (readability & reusability)
+// - create a sepetate component for buttons, lists, ... (readability & reusability & easier to maintain on bigger projects)
+//    - This way you could also split your logic and styling (css)
+// - for naming classes we generally user BEM standard in our projects https://sparkbox.com/foundry/bem_by_example
+
+function App() {
+    const [cityName, setCityName] = useState("Antwerp");
+    const [fetchErrorCity, setFetchErrorCity] = useState(false);
+    const [weather, setWeather] = useState({
+        temperature: "", //An empty string as default value is not a good practice, you can use null here. especially if the final data is a number
+        feelsLike: "",
+        conditionText: "",
+        locationText: "",
+        localtime: "",
+        dayOfWeek: "",
+        humidity: "",
+        wind_kph: "",
+        icon: "",
+        forecast: [],
+    });
+    const [previousResearch, setPreviousResearch] = useState(() => {
+        const storedData = localStorage.getItem("previousResearch");
+        return storedData ? JSON.parse(storedData) : [];
+    });
+    useEffect(() => {
+        fetchData(cityName);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    useEffect(() => {
+        localStorage.setItem("previousResearch", JSON.stringify(previousResearch));
+    }, [previousResearch]);
+    const fetchData = async (cityName) => {
+        setFetchErrorCity(false)
+
+        const options = {
+            method: 'GET',
+            params: {q: cityName},
+            headers: {
+                'X-RapidAPI-Key': '734bc29f81msh85bd8e1dea8c988p1bb9c2jsn3133e9a810a3', //Try to put your key in an .env variable 
+                'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+            },
+        };
+
+        try {
+            const response = await fetch(
+                `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${cityName}&days=3`,
+                options
+            );
+            const data = await response.json();
+            const {current, location, forecast} = data;
+            const {temp_c: tempC, feelslike_c: feelsLikeC, humidity, wind_kph} = current;
+            const {name, country, localtime: localtimeText} = location;
+            const date = new Date(localtimeText);
+            const formatDate = (dateString) => {
+                const date = new Date(dateString);
+                return date.toLocaleDateString('en-US', {day: 'numeric', month: 'short'});
+            };
+            const formatDayOfWeek = (dateString) => {
+                const date = new Date(dateString);
+                return date.toLocaleDateString('en-US', {weekday: 'long'});
+            };
+            const formatForecast = (forecast) => {
+                const forecastDays = forecast.forecastday;
+                return forecastDays.map((day) => {
+                    const date = new Date(day.date);
+                    const dayOfWeek = date.toLocaleDateString('en-US', {weekday: 'short'});
+                    const maxTemp = day.day.maxtemp_c;
+                    const minTemp = day.day.mintemp_c;
+                    const icon = day.day.condition.icon;
+                    return {dayOfWeek, icon, maxTemp: `${maxTemp}C`, minTemp: `${minTemp}C`};
+                });
+            };
+
+            const formattedDateTime = formatDate(date);
+            const dayOfWeek = formatDayOfWeek(date);
+            const conditionText = current.condition.text;
+            const icon = current.condition.icon;
+            const formattedForecast = formatForecast(forecast);
+            setWeather({
+                cityName,
+                temperature: `${tempC}°C`, //It's not ideal to store °C in the string. This can be added in the HTML. (for example more difficult to maintain if user can switch between celius and fahrenheit)
+                feelsLike: `${feelsLikeC}°C`, //same
+                humidity: `${humidity}%`, // same for %
+                wind_kph: `${wind_kph} km/h`, // same for km/h
+                conditionText,
+                icon,
+                name,
+                country,
+                locationText: formattedDateTime,
+                localtime: formattedDateTime,
+                dayOfWeek,
+                forecast: formattedForecast,
+            });
+                if (previousResearch.length >= 5) {
+                    let updatedResearch = [...previousResearch, cityName].slice(-5);
+                    setPreviousResearch(updatedResearch);
+                } else {
+                    setPreviousResearch(previousResearch => [...previousResearch, cityName])
+                }
+
+                localStorage.setItem("previousResearch", JSON.stringify(previousResearch));
+        } catch (err) {
+            setFetchErrorCity(true)
+            console.error('City not found')
+        }
+    };
+    const handleCityChange = (event) => {
+        setCityName(event.target.value);
+    };
+
+    const handleSearchClick = (cityName) => {
+        fetchData(cityName);
+    };
+    const handleCityChangeViaStorage = (cityName) => {
+        setCityName(cityName);
+        handleSearchClick(cityName)
+    }
+    return (
+        <Fragment> {/*You can use <></> instead of <Fragment></Fragment>, same output but no need for extra import*/}
+            <div className="container">
+                <div className="weather-side">
+                    <div className="weather-gradient"><img src={weather.icon} alt=""/></div>
+                    <div className="date-container">
+                        <p className="location">{weather.name} - {weather.country}</p>
+
+                        <h2 className="date-dayname">{weather.dayOfWeek}</h2>
+                        <i className="location-icon" data-feather="map-pin"></i>
+                        <span className="location">{weather.locationText}</span>
+                    </div>
+                    <div className="weather-container"><i className="weather-icon" data-feather="sun"></i>
+                        <h1 className="weather-temp">{weather.temperature}</h1>
+                        <h3 className="weather-desc">{weather.conditionText}</h3>
+                    </div>
+                </div>
+                <div className="info-side">
+                    <div className="today-info-container">
+                        <div className="today-info">
+                            <div className="precipitation"><span className="title">WIND</span><span
+                                className="value">{weather.wind_kph}</span>
+                                <div className="clear"></div>
+                            </div>
+                            <div className="humidity"><span className="title">HUMIDITY</span><span
+                                className="value">{weather.humidity}</span>
+                                <div className="clear"></div>
+                            </div>
+                            <div className="wind"><span className="title">FELLING LIKE</span><span
+                                className="value">{weather.feelsLike}</span>
+                                <div className="clear"></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="week-container today-info">
+                        <span className="title">NEXT THREE DAYS</span>
+                        <ul className="week-list">
+                            {weather.forecast && weather.forecast.map((day) => (
+                                <li key={day.dayOfWeek}>
+                                    <i className="day-icon" data-feather="cloud"><img src={day.icon} alt=""/></i>
+                                    <span className="day-name">{day.dayOfWeek}</span>
+                                    <span className="day-temp">{day.maxTemp}</span>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className="location-container">
+
+                        <input
+                            type="text"
+                            placeholder="Enter a city name"
+                            id="search-btn"
+                            className={`searchTerm${fetchErrorCity ? ' errorCity' : ''}`}
+                            value={cityName}
+                            onChange={handleCityChange}
+                        />
+                        <button className="location-button" onClick={() => handleSearchClick(cityName)}>
+                            <span>Change location</span>
+                        </button>
+                        {
+                            fetchErrorCity && (
+                                <p className="text-error-city">
+                                    Wrong city
+                                </p>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
+            {/* eslint-disable-next-line array-callback-return */}
+            {previousResearch.length > 0 &&
+                <section className="container_research">
+                    <h2>5 Previous research</h2>
+                    <ul>
+                        {previousResearch.map((element, index) => (
+                            <li onClick={() => handleCityChangeViaStorage(element)} key={index}>{element}</li>
+                        ))}
+                    </ul>
+                </section>
+            }
+        </Fragment>
+    );
+}
+
+export default App;
